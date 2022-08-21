0e156db331c06da558a5c9b749dcb38265164a70
avagyansamvel <avagyansamvel07@gmail.com>
2022-08-21 17:28:14 +0400

    Add User Controller

---
+++ JwtApp/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using JwtApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JwtApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("Admins")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AdminsEndpoint()
+        {
+            var user = GetCurrentUser();
+
+            return Ok($"Hi {user.GivenName}, you are {user.Role}");
+        }
+
+        [HttpGet("Public")]
+        public IActionResult Public()
+        {
+            return Ok("Hi, you're on public property");
+        }
+
+        private User GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new User
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+
+            return null;
+        }
+    }
+}
